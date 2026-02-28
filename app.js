d829690373560afcf13af45d4beab856def3a937
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-02-28 15:03:45 +0530

    updated url

--- app.js
+++ app.js
@@ -14,9 +14,22 @@ import analyticsRoutes from './routes/analytics.routes.js';
 dotenv.config();
 
 const app = express();
-
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:8080",
+  "http://localhost:5173",
+];
 app.use(cors({
-  origin: true,
+  origin:  function (origin, callback) {
+
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
